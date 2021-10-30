497d08e4993912d64e93381c9f66d17b2eb51388
yuyanmin <1812006192@qq.com>
2021-10-30 16:46:39 +0800

    feat: gitigmore

--- 02.打包样式文件/webpack.config.js
+++ 02.打包样式文件/webpack.config.js
@@ -1,4 +1,13 @@
+/*
+  webpack.config.js  webpack的配置文件
+    作用: 指示 webpack 干哪些活（当你运行 webpack 指令时，会加载里面的配置）
+
+    所有构建工具都是基于nodejs平台运行的~模块化默认采用commonjs。
+*/
+
+// resolve用来拼接绝对路径的方法
 const { resolve } = require("path");
+
 module.exports = {
   // 模式
   mode: "development",
@@ -8,6 +17,7 @@ module.exports = {
   output: {
     // 输出文件名
     filename: "built.js",
+    // __dirname nodejs的变量，代表当前文件的目录绝对路径
     path: resolve(__dirname, "build"),
   },
   // loader的配置
